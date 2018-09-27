df4f2d57afa4e1c18a38f89a466145bb40a31347
guosanbin <guosanbin@linkeye.com>
2018-09-27 18:15:40 +0800

    use blockchain.info to getblockcount

--- src/main/java/com/biwork/service/Impl/BlockNumberServiceImpl.java
+++ src/main/java/com/biwork/service/Impl/BlockNumberServiceImpl.java
@@ -20,50 +20,45 @@ import org.web3j.protocol.http.HttpService;
 import com.biwork.entity.BlockNumber;
 import com.biwork.service.BlockNumberService;
 import com.biwork.util.HttpUtil;
-import com.neemre.btcdcli4j.core.client.BtcdClient;
-import com.neemre.btcdcli4j.core.client.BtcdClientImpl;
 
-import java.util.Map;
+import com.biwork.exception.BusiException;
 
 @Service("BlockNumberService")
 public class BlockNumberServiceImpl implements BlockNumberService {
 
 	static Logger log = LoggerFactory.getLogger(BlockNumberService.class);
 	private static final String PRO_URL = "https://mainnet.infura.io/PVMw2QL6TZTb2TTgIgrs";
+	private static final String BCI_URL = "https://blockchain.info/";
 
 	@Override
 	public BlockNumber getEthBlockNumber() throws Exception {
 		BlockNumber bn = new BlockNumber();
 		Web3j web3j = Web3j.build(new HttpService(PRO_URL, true));
-    	BigInteger bn_bi = null;
+		BigInteger bn_bi = null;
 		try {
 			bn_bi = web3j.ethBlockNumber().send().getBlockNumber();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-        bn.setBlockNumber(bn_bi.toString(10));
-   	
+		bn.setBlockNumber(bn_bi.toString(10));
+
 		return bn;
 	}
 
 	@Override
 	public BlockNumber getBtcBlockNumber() throws Exception {
 		BlockNumber bn = new BlockNumber();
-		
-		PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
-		CloseableHttpClient httpProvider = HttpClients.custom().setConnectionManager(cm)
-				.build();
-		Properties nodeConfig = new Properties();
-		InputStream is = this.getClass().getResourceAsStream("/node_config.properties");
-		nodeConfig.load(is);
-		is.close();
 
-		BtcdClient client = new BtcdClientImpl(httpProvider, nodeConfig);
-		Integer blockCount = client.getBlockCount();
-		
-		bn.setBlockNumber(blockCount.toString());
-		
+		String rsp = "";
+		try {
+			rsp = HttpUtil.testGet(BCI_URL + "/q/getblockcount");
+		} catch (Exception e) {
+			throw new BusiException(Integer.toString(e.hashCode()), e.getMessage());
+		}
+
+		bn.setBlockNumber(rsp);
+
 		return bn;
 	}
 }
